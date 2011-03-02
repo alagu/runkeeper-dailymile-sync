8aad219a54fe32a82ba553b1656d5c6b26b07774
Chris Moore <cdmwebs@gmail.com>
2011-03-02 14:10:51 -0500

    add admin user area

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rkdm::Application.routes.draw do
   match "/signout" => "sessions#destroy", :as => :signout
   match "/account" => "users#edit", :as => :account
   match "/admin" => "admin#show", :as => :admin
+  namespace :admin do
+    resources :users, :only => :index
+  end
   resources :users, :only => [:edit, :update]
   root :to => "dashboard#show"
 end
