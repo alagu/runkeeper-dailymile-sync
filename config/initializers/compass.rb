f92f47169b08a080c0a41558345621a73bf36426
Chris Moore <cdmwebs@gmail.com>
2011-03-02 04:28:44 -0500

    compile css to tmp/ & ignore compiled css

--- config/initializers/compass.rb
+++ config/initializers/compass.rb
@@ -1,3 +1,8 @@
 require 'compass'
 require 'compass/app_integration/rails'
+require "fileutils"
+
 Compass::AppIntegration::Rails.initialize!
+
+FileUtils.mkdir_p(Rails.root.join("tmp", "stylesheets", "compiled"))
+Rails.application.middleware.insert_before('Rack::Lock', 'Rack::Static', :root => "tmp/", :urls => ["/stylesheets/compiled"])
