37707d5ccfac94e4e554953668f1431cc036a0aa
Alagu <allagappan@gmail.com>
2012-09-18 13:19:31 -0700

    Change mailer

--- app/mailers/mailer.rb
+++ app/mailers/mailer.rb
@@ -1,9 +1,11 @@
 class Mailer < ActionMailer::Base
   def notify(subject, message)
-      mail(:to      => "alagu@alagu.net", 
-           :from    => "alagu@alagu.net",
-           :subject => subject) do |format|
-        format.text { render :text => message }
-      end
+    puts "Sending #{subject} #{message}"
+    
+    mail(:to      => "alagu@alagu.net", 
+         :from    => "alagu@alagu.net",
+         :subject => subject) do |format|
+      format.text { render :text => message }
+    end
   end
 end
