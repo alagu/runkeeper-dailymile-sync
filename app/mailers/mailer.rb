2d2f400397565d8ad2e26b9112d298f31c6fcc51
Alagu <allagappan@gmail.com>
2012-09-18 13:05:35 -0700

    Mailer works

--- app/mailers/mailer.rb
+++ app/mailers/mailer.rb
@@ -1,6 +1,9 @@
 class Mailer < ActionMailer::Base
   def notify(subject, message)
-      mail(:to => "alagu@alagu.net", :from => "alagu@alagu.net",
-           :subject => subject, :text => message)
+      mail(:to      => "alagu@alagu.net", 
+           :from    => "alagu@alagu.net",
+           :subject => subject) do |format|
+        format.text { render :text => message }
+      end
   end
 end
